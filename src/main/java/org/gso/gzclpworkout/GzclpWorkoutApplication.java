baa668367b4ae680a7710b48f2ff9e33aa659424
Elzenif <shigo.germain@gmail.com>
2018-10-27 13:45:11 +0200

    Initial commit

---
+++ src/main/java/org/gso/gzclpworkout/GzclpWorkoutApplication.java
@@ -0,0 +1,12 @@
+package org.gso.gzclpworkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzclpWorkoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GzclpWorkoutApplication.class, args);
+	}
+}
